fb8d2be31f0107fd4a0675109dba9930e21ded2a
Ryan Billings <rbilling@andrew.cmu.edu>
2011-11-22 01:06:03 -0500

    Bug fix

--- workout/app/models/day.rb
+++ workout/app/models/day.rb
@@ -1,11 +1,8 @@
 class Day < ActiveRecord::Base
-  attr_accessible :plan_id, :date, :duration, :complete
+  attr_accessible :plan_id, :date, :duration, :complete, :day
   belongs_to :plan
   has_many :excersises
-  has_event_calendar :start_at_field => 'date', :end_at_field => 'date', :name_field => 'plan_id'
+  has_event_calendar :start_at_field => 'date', :end_at_field => 'date'
 
-  def name
-    "Chest"
-  end
 
 end
