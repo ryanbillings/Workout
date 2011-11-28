646beb7d6b80a5d6bae002ed46cdb9caaadf6619
Ryan Billings <rbilling@andrew.cmu.edu>
2011-11-28 18:03:51 -0500

    Deleted files manually

--- workout/test/unit/excersise_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ExcersiseTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Excersise.new.valid?
-  end
-end
--- workout/test/unit/workout_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class WorkoutTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Workout.new.valid?
-  end
-end
